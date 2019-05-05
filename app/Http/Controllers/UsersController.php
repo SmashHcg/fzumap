8dbbd4d511e29380a4736673212b465ff76c3888
SmashHcg <673035915@qq.com>
2019-05-05 17:02:03 +0000

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,11 +4,33 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     public function create()
     {
         return view('users.create');
+    }
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|min:8|max:16',
+            'password' => 'required|confirmed|min:6|max:16'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'password' => bcrypt($request->password),
+        ]);
+
+        session()->flash('success', '欢迎使用福大校园地图APP~');
+        return redirect()->route('users.show', [$user]);
     }
 }
