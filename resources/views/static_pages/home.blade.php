ab7c2a868c638bfaec7f3b51c34358c72dfcd3eb
SmashHcg <673035915@qq.com>
2019-05-05 13:46:29 +0000

    Finish layout and routes

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.default')
+
 @section('content')
-  <h1>主页</h1>
+  <div class="jumbotron">
+    <h1>福大校园地图 APP</h1>
+    <p class="lead">
+      你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training/5.5">福大校园地图APP</a> 的示例项目主页。
+    </p>
+    <p>
+      开始你的福大之旅吧！
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在创建用户</a>
+    </p>
+  </div>
 @stop
---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title', '创建用户')
+
+@section('content')
+<h1>创建用户</h1>
+@stop
